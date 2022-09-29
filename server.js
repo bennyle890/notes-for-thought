6e5a4dd86dc0693074d01fba3626f376da79e5b4
Benny <benny.le890@gmail.com>
2022-09-29 14:35:56 -0400

    I THINK I GOT IT

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
 const notes = [];
 
