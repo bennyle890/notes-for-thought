11877a4578a4200d053bbe878c38896dc8484a37
Benny <benny.le890@gmail.com>
2022-09-29 14:01:27 -0400

    sdfasd

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static('public'));
 
 const notes = [];
 
